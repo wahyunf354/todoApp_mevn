c495fc9adea4c5d957644bc0e24c1a2e21b56148
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-05-31 22:47:27 +0700

    add file server.js

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// define express
+const app = express()
+
+// define list url parmission cors
+const whiteList = [
+  'http://localhost:8081'
+]
+
+// difine validate CORS Url
+const corsOption = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not Allowed CORS'))
+    }
+  }
+}
+
+// use CORS in App
+app.use(cors(corsOption))
+
+// use body-parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// define try route in server
+app.get('/', (res, req) => {
+  res.json({
+    message: 'Welcome To MEVN'
+  })
+})
+
+// define port url
+const PORT = process.env.PORT || 8080
+
+// listen server ini post url
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
