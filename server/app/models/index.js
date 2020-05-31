993daad000b1ff7bdddfc9c29d692db5592d99e2
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-05-31 23:00:54 +0700

    add index.js in folder models

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const dbConfig = require('../config/db.config.js')
 
+// define config to sequelize
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.DIALECT,
@@ -12,3 +13,11 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     idle: dbConfig.pool.idle
   }
 })
+
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+db.todos = require('./todo.models.js')(sequelize, Sequelize)
+
+module.exports = db
