de6572064043a2a23b1bae7bba98c7ddf56d2328
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-06-01 06:26:57 +0700

    add method create, getAll, getOne, Delete in API and router

--- server/app/controllers/todo.controller.js
+++ server/app/controllers/todo.controller.js
@@ -1,14 +1,14 @@
 const db = require('../models')
 const Todo = db.todos
-const Op = db.Sequelize.Op
+const { Op } = db.Sequelize
 
 // Method Create
 exports.create = (req, res) => {
   // Validasi Todo
   if (!req.body.title) {
     res.status(400).send({
-      message: `Content cann't be empty`
-    }) 
+      message: 'Content cann\'t be empty'
+    })
   }
 
   // Create Todo
@@ -20,12 +20,67 @@ exports.create = (req, res) => {
   }
 
   Todo.create(todo)
-    .then((result => {
+    .then(result => {
       res.send(result)
-    }))
+    })
     .catch(err => {
       res.status(500).send({
         message: err.message || 'Some error occured while creating the post'
       })
     })
+}
+
+// Method get all todo
+exports.findAll = (req, res) => {
+  const { title } = req.query
+  let condition = title ? { title: { [Op.like]: `%${title}%` } } : null
+  if (!condition) {
+    const { description } = req.query
+    condition = description ? { description: { [Op.like]: `%${description}%` } } : null
+  }
+
+  Todo.findAll({ where: condition })
+    .then(result => res.send(result))
+    .catch(err => res.status(500).send({
+      message: err.message || 'Some error occured while find post'
+    }))
+}
+
+// Method get one todo by ID
+exports.findById = (req, res) => {
+  const { id } = req.params
+
+  Todo.findByPk(id)
+    .then(result => {
+      if (result) {
+        res.send(result)
+      } else {
+        res.send({
+          message: 'Todo is not found'
+        })
+      }
+    })
+    .catch(err => res.status(500).send({
+      message: `Error retrieving todo with id = ${id}`,
+      error: err
+    }))
+}
+
+// Method Delete
+exports.deleteById = (req, res) => {
+  const { id } = req.params
+
+  Todo.destroy({ where: { id } })
+    .then(result => {
+      if (result === 1) {
+        res.send({
+          message: 'Todo was deleted successfully'
+        })
+      } else {
+        res.send({
+          message: 'Cann\'t deleted Todo'
+        })
+      }
+    })
+    .catch(err => res.status(500).send(err))
 }
--- server/app/routers/todo.routes.js
+++ server/app/routers/todo.routes.js
@@ -1,9 +1,18 @@
 module.exports = (app) => {
-  const  todo = require('../controllers/todo.controller.js')
+  const todo = require('../controllers/todo.controller.js')
   const router = require('express').Router()
 
   // Create a new Todo
   router.post('/', todo.create)
+
+  // Find all todo
+  router.get('/', todo.findAll)
+
+  // Find todo by id
+  router.get('/:id', todo.findById)
+
+  // delete by id
+  router.delete('/:id', todo.deleteById)
 
   app.use('/api/todo', router)
 }
